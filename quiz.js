6c152c25661d4c1dc62e9a93c2f20156b83a613c
Alvin karani <alvinkaranimudembei@gmail.com>
2025-06-13 17:29:42 +0300

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+
+var correctAnswer = "4"
+
+userAnswer = document.querySelector()("#userAnswer").value;
+if (userAnswer === correctAnswer) {
+    document.querySelector("#result").innerHTML = "Correct! Well done.";
+    document.getElementById('feedback').innerHTML = "Correct! Well done.";
+
+} else {
+    document.querySelector("#result").innerHTML = "That's incorrect,try again";
+}   document.getElementById('feedback').innerHTML = "That's incorrect,try again";
+
+}  
